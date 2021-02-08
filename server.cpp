9992c55e49d3c2e25e2a8afe06ac9cc26066be61
unknown <nmam@msystechnologies.com>
2021-02-08 23:52:17 +0530

    initial

--- server.cpp
+++ server.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-#include <csi.grpc.pb.h>
 #include <csi.pb.h>
+#include <csi.grpc.pb.h>
+
+#include <iostream>
 
 int main(void)
 {
-  
+  csi::v1::Identity identity;
+
+
   return 0;
 }

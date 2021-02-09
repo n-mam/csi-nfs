2427be89fd8e6c9956a82574c58ebda4ec0801d9
unknown <nmam@msystechnologies.com>
2021-02-09 09:40:52 +0530

    initial

---
+++ INCLUDE/Identity.hpp
@@ -0,0 +1,13 @@
+#ifndef IDENTITY_HPP
+#define IDENTITY_HPP
+
+#include <csi.pb.h>
+#include <csi.grpc.pb.h>
+
+class IdentityService : public csi::v1::Identity::Service
+{
+
+};
+
+
+#endif
---
+++ INCLUDE/Node.hpp
@@ -0,0 +1,13 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <csi.pb.h>
+#include <csi.grpc.pb.h>
+
+class NodeService : public csi::v1::Identity::Service
+{
+
+};
+
+
+#endif

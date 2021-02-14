61cc20a9dad26f39b699e345b9e983aaff6400b1
unknown <nmam@msystechnologies.com>
2021-02-14 10:03:42 +0530

    dockerfile

--- DEPLOY/Deploy.bat
+++ DEPLOY/Deploy.bat
@@ -19,6 +19,16 @@ minikube kubectl -- get rs -n %NAMESPACE%
 
 minikube kubectl -- get pods -n %NAMESPACE%
 
+REM list all pod containers
+
 minikube kubectl -- get pods POD_NAME_HERE -o jsonpath='{.spec.containers[*].name}' -n %NAMESPACE%
+
+REM pod's container logs
+
+minikube kubectl -- logs POD_NAME_HERE -c POD_CNTAINER_NAME_HERE
+
+REM mount /home/nmam/csi-nfs/ repo into minikube's vm 
+
+minikube mount /home/nmam/csi-nfs/:/home/docker/csi-nfs
 
 ENDLOCAL

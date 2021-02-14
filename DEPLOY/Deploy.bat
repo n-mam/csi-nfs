e490d09aa9890c9505daac7915eba50388214477
unknown <nmam@msystechnologies.com>
2021-02-14 18:11:27 +0530

    refactor

--- DEPLOY/Deploy.bat
+++ DEPLOY/Deploy.bat
@@ -1,39 +1,39 @@
 @echo off
 SETLOCAL
 
-SET NAMESPACE=xxx
+REM kubectl delete namespace xxx
 
-kubectl delete namespace xxx
+REM kubectl apply -f common.yaml
 
-kubectl apply -f common.yaml
+REM kubectl apply -f controller.yaml
 
-kubectl apply -f controller.yaml
+REM kubectl get deploy -n xxx
 
-kubectl get deploy -n xxx
+REM kubectl rollout status deploy/xxx-controller-deployment -n xxx
 
-kubectl rollout status deploy/xxx-controller-deployment -n xxx
+REM kubectl describe deploy/xxx-controller-deployment -n xxx
 
-kubectl describe deploy/xxx-controller-deployment -n xxx
+REM kubectl get rs -n xxx
 
-kubectl get rs -n xxx
-
-kubectl get pods -n xxx
+REM kubectl get pods -n xxx
 
 REM list all pod containers
 
-kubectl get pods POD_NAME_HERE -o jsonpath='{.spec.containers[*].name}' -n xxx
+REM kubectl get pods POD_NAME_HERE -o jsonpath='{.spec.containers[*].name}' -n xxx
 
 REM pod's container logs
 
-kubectl logs POD_NAME_HERE -c POD_CONTAINER_NAME_HERE -n xxx
+REM kubectl logs POD_NAME_HERE -c POD_CONTAINER_NAME_HERE -n xxx
 
 REM mount /home/nmam/csi-nfs/ repo into minikube's vm 
 
-minikube mount /home/nmam/csi-nfs/:/home/docker/csi-nfs
+REM minikube mount /home/nmam/csi-nfs/:/home/docker/csi-nfs
 
 REM eval $(minikube docker-env)
 REM docker build -t my-image .
 REM minikube ssh
 REM docker images my-image
+
+REM  docker run -i debian:buster-slim /bin/sh
 
 ENDLOCAL
